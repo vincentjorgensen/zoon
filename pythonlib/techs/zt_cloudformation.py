5ae33982f7cf10a34bc9a067fcbffd4eedb1de40
Vincent Jorgensen <vincent.jorgensen@spongecell.com>
2015-01-14 16:29:56 -0800

    mappings and params

--- pythonlib/techs/zt_cloudformation.py
+++ pythonlib/techs/zt_cloudformation.py
@@ -10,14 +10,23 @@ def CloudFormation:
 
     def __init__(self, **kw_params):
         self.region = kw_params['region']
-        self.stack_name = kw_params['name']
+        self.stack_name = kw_params['stack_name']
+        self.app_name = kw_params['app_name']
+        self.app_type = kw_params['app_type']
         self.size = kw_params['size']
         self.env = kw_params['env']
         self.mode = kw_params['mode']
+        self.stack_name = self.region + '-' + self.app_type + '-' + self.app_name
 
         self.zoonrc = Zoonrc(deployment_environment=self.env)
         self.metroon = metroon(deployment_environment=self.env)
         self.conn = self._create_cf_connection()
+
+        if self.region in ['us-east-1','us-west-1','us-west-2','eu-west-1']:
+            self.azs = 3
+        else:
+            self.azs = 2
+        self.azsize = self.size/self.azs
 
     def create(self):
         self.conn.create_stack(
@@ -35,19 +44,78 @@ def CloudFormation:
     def status(self):
 
     def _template(self):
+        template={
+          "Parameters" = {},
+          "Mappings" = {},
+          "Resources" = {}
+        }
+        parameters = [
+          ("KeyName", "String"), 
+          ("AvailabilityZoneA", "CommaDelimitedList"),
+          ("AvailabilityZoneB", "CommaDelimitedList"),
+          ("ClientSecurityGroup", "String"),
+          ("AZSize","Number"),
+          ("AvailabilityZoneC","CommaDelimitedList"),
+          ("AZSize","Number"),
+          ("ConsulDataCenter","String"),
+          ("ConsulEndpoint","String"),
+          ("DeployEnv","String"),
+          ("ClusterName","String"),
+          ("VPCName","String"),
+          ("SubnetA","CommaDelimitedList"),
+          ("SubnetB","CommaDelimitedList")
+          ("InstanceType","String")
+        ]
+
+        if self.azs == 3:
+          parameters.append(("AvailabilityZoneC", "CommaDelimitedList"))
+          pramaters.append(("SubnetC","CommaDelimitedList"))
+
+        if self.mode == "create":
+          parameters.append(("AdminSecurityGroup", "String"))
+          parameters.append(("VPCName", "String"))
+
+       for tup in parameters:
+        template['Parameters'][tup[0]] = {"Type": tup[1]}
+
+        mappings = {
+          "RegionToAmi" = {},
+          "AwsAccountMap": {
+            "076336089575": {
+              "Bucket": "spongecell-devops"
+            }
+          }
+        }
+
+       # Supported regions format:
+       # 
+       supportedregions = metroon.get(self.env + '-supportedregions')
+
+       for reg in supportedregions:
+           mappings['RegionToAmi'][reg] = metroon.get(self.env + '-' + reg + '-hvm')
+
+
 
     def _params(self):
         params = [
             {"ParameterKey": "KeyName"              ,     "ParameterValue": metroon.get(self.env + '-keypair')},
-            {"ParameterKey": "Subnets"              ,     "ParameterValue": metroon.get(self.env + '-subnets')},
-            {"ParameterKey": "AvailabilityZones"    ,     "ParameterValue": metroon.get(self.env + '-azs')},
+            {"ParameterKey": "SubnetA"              ,     "ParameterValue": metroon.get(self.env + '-subnetA')},
+            {"ParameterKey": "SubnetB"              ,     "ParameterValue": metroon.get(self.env + '-subnetB')},
+            {"ParameterKey": "AvailabilityZoneA"    ,     "ParameterValue": metroon.get(self.env + '-azA')},
+            {"ParameterKey": "AvailabilityZoneB"    ,     "ParameterValue": metroon.get(self.env + '-azB')},
             {"ParameterKey": "ClientSecurityGroup"  ,     "ParameterValue": metroon.get(self.env + '-clientsg')},
-            {"ParameterKey": "ClusterSize"          ,     "ParameterValue": self.size },
+            {"ParameterKey": "DeployEnv          "  ,     "ParameterValue": self.env },
+            {"ParameterKey": "AZSize"               ,     "ParameterValue": self.azsize },
             {"ParameterKey": "ClusterName"          ,     "ParameterValue": self.env } 
         ]
+        if self.azs == 3:
+            params.append({"ParameterKey": "SubnetC"              ,     "ParameterValue": metroon.get(self.env + '-subnetC')})
+            params.append({"ParameterKey": "AvailabilityZoneC"    ,     "ParameterValue": metroon.get(self.env + '-azC')})
+
         if self.mode == "create":
             params.append({"ParameterKey": "AdminSecurityGroup"   ,     "ParameterValue": metroon.get(self.env + '-adminsg')})
-            params.append({"ParameterKey": "VpcId"                ,     "ParameterValue": metroon.get(self.env + '-vpc')})
+            params.append({"ParameterKey": "VPCName"              ,     "ParameterValue": metroon.get(self.env + self.region + '-vpcname')})
+
         return params
 
 def _create_cf_connection(self):
